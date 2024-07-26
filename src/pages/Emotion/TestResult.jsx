a6bc9887a362a3f61017905333aa467be7d6e9ef
Juhee <pjh402941@gmail.com>
2024-07-26 17:08:08 +0900

    feat: 성격 검사 결과 ui

--- src/pages/Emotion/TestResult.jsx
+++ src/pages/Emotion/TestResult.jsx
@@ -31,8 +31,54 @@ const Title = styled.h1`
   font-weight: 700;
 `;
 
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2.5rem;
+  margin-top: 4.94rem;
+  #title {
+    padding: 0.9375rem 13.0625rem 0.9375rem 13rem;
+    border-radius: 1.5625rem;
+    box-shadow: 0px 0px 14.9px 0px rgba(53, 100, 140, 0.35);
+    color: #35648c;
+    font-size: 1.5rem;
+    font-weight: 500;
+    #bold {
+      font-weight: 700;
+    }
+  }
+  #nav {
+    display: flex;
+    gap: 2.5rem;
+    margin-top: 2.5rem;
+    margin-bottom: 24rem;
+
+    button {
+      border: none;
+      display: flex;
+      width: 21.875rem;
+      padding: 1.125rem 0rem 1.0625rem 0rem;
+      justify-content: center;
+      border-radius: 0.9375rem;
+      background: #fff;
+      box-shadow: 0px 0px 14.9px 0px rgba(53, 100, 140, 0.35),
+        0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+      color: #35648c;
+      font-size: 1.25rem;
+      font-weight: 500;
+    }
+    #goMypage {
+      color: white;
+      background: #35648c;
+    }
+  }
+`;
+
+const ImgWrapper = styled.div``;
+
 const TestResult = () => {
   const navigate = useNavigate();
+  const [type, setType] = useState(1); //1, 2, 3, 4
 
   const navigateTo = (path) => {
     navigate(path);
@@ -46,7 +92,30 @@ const TestResult = () => {
           src={`${process.env.PUBLIC_URL}/assets/logo/LogoGra.png`}
           width="60px"
         />
-        <Title>성격 검사 진단</Title>
+        <Title>00님의 성격 검사 결과</Title>
+
+        <Content>
+          <ImgWrapper>
+            <img
+              src={`${process.env.PUBLIC_URL}/assets/testResult/1.png`}
+              width="300px"
+            />
+          </ImgWrapper>
+          <div id="title">
+            휴일은 <span id="bold">하우스키퍼</span>유형
+          </div>
+          <div id="message">
+            휴일은 하우스키퍼 유형은 주로 휴일 날 시간을 집에서 보내고 집에서
+            휴식을 취해야
+            <br /> 진정한 휴식이라고 생각하는 유형입니다.
+            <br /> 휴일은 하우스키퍼 유형인 00님을 위해 집에서 주로 즐기는
+            맞춤형 쉼을 추천드릴게요!
+          </div>
+          <div id="nav">
+            <button id="retry">재진단</button>
+            <button id="goMypage">마이페이지로 이동</button>
+          </div>
+        </Content>
       </Main>
       <Footer />
     </Container>
